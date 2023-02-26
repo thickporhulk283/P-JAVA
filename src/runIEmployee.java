1e78f3c3b63e95c7458798a144a5ad09861588c0
unknown <hn3206559@gmail.com>
2023-02-26 15:51:58 +0700

    update

--- src/runIEmployee.java
+++ src/runIEmployee.java
@@ -2,11 +2,11 @@ public class runIEmployee {
     public static void main(String[] args) {
 		IEmployee employee1 = new PartTimeEmployee("Lam", 20000, 4);
 		System.out.println("Name: " + employee1.getName());
-		System.out.println("Salary per day: " + employee1.calculateSalary());
+		System.out.println("Salaryday: " + employee1.calculateSalary());
 
 		IEmployee employee2 = new FullTimeEmployee("Thai", 20000);
 		System.out.println("Name: " + employee2.getName());
-		System.out.println("Salary per day: " + employee2.calculateSalary());
+		System.out.println("Salaryday: " + employee2.calculateSalary());
 	}
 }
     
